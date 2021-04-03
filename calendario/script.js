434379654ae7352fc1088c0177393c4d37e4ca32
Vinícius Carvalho Ribeiro Quirino <cabohdmiusb@hotmail.com>
2021-04-03 15:30:58 -0300

    maio ok

--- calendario/script.js
+++ calendario/script.js
@@ -12,7 +12,7 @@ function showtime(){
     var month = time.getMonth()
     var year = time.getFullYear()   
 
-    month = 3;
+    month = 4;
 
     switch(month){
         case 0 :
@@ -367,49 +367,58 @@ function showtime(){
                     </thead>
                     <tbody>
                         <tr>
+                            <td class="text-muted">26</td>
+                            <td class="text-muted">27</td>
+                            <td class="text-muted">28</td>
                             <td class="text-muted">29</td>
                             <td class="text-muted">30</td>
                             <td class="text-muted">31</td>
                             <td><a href="https://pt.wikipedia.org/wiki/Dia_do_Trabalhador" class="text-danger" target="_blank"><button><strong>1</strong></a></td>
+                        </tr>
+                        <tr>
                             <td>2</td>
                             <td>3</td>
                             <td>4</td>
-                        </tr>
-                        <tr>
                             <td>5</td>
                             <td>6</td>
                             <td>7</td>
                             <td>8</td>
+                        </tr>
+                        <tr>
                             <td><a href="https://pt.wikipedia.org/wiki/Dia_das_M%C3%A3es" class="text-danger" target="_blank"><button><strong>9</strong></a></td>
                             <td>10</td>
                             <td>11</td>
-                        </tr>
-                        <tr>
                             <td>12</td>
                             <td>13</td>
                             <td>14</td>
                             <td>15</td>
+                        </tr>
+                        <tr>
                             <td>16</td>
                             <td>17</td>
                             <td>18</td>
-                        </tr>
-                        <tr>
                             <td>19</td>
                             <td>20</td>
                             <td>21</td>
                             <td>22</td>
+                        </tr>
+                        <tr>
                             <td>23</td>
                             <td>24</td>
                             <td>25</td>
-                        </tr>
-                        <tr>
                             <td>26</td>
                             <td>27</td>
                             <td>28</td>
                             <td>29</td>
+                        </tr>
+                        <tr>
                             <td>30</td>
                             <td>31</td>
                             <td class="text-muted">1</td>
+                            <td class="text-muted">2</td>
+                            <td class="text-muted">3</td>
+                            <td class="text-muted">4</td>
+                            <td class="text-muted">5</td>
                         </tr>
                     </tbody>
                 </table>
