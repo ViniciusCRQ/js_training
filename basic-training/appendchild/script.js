af0e9e0949c75a669e2a0b56bc9a3587d481f546
Vinícius Carvalho Ribeiro Quirino <cabohdmiusb@hotmail.com>
2021-04-03 20:48:18 -0300

    Last Update

--- basic-training/appendchild/script.js
+++ basic-training/appendchild/script.js
@@ -1,14 +1,53 @@
 let days = []
+let weekdays = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab']
 
 for ( let x = 1; x<32; x++){
     days.push(Number(x))
 }
 
+let tabela = document.createElement('table')
+
 let body = document.getElementsByTagName('body')
 
-console.log(days)
+body[0].appendChild(tabela)
 
-function createtr() {
-    let trdays = document.createElement('tr')
+let tabelanobody = document.getElementsByTagName('table');
+
+let tabelahead = document.createElement('thead');
+
+let tabelabody = document.createElement('tbody');
+
+let tabelaheadnobody = document.getElementsByTagName('thead')
+let tabelabodynobody = document.getElementsByTagName('tbody')
+
+tabelanobody[0].appendChild(tabelahead)
+
+
+let tabelatr = document.createElement('tr')
+   
+tabelaheadnobody[0].appendChild(tabelatr)
+
+let tabelatrnobody = document.getElementsByTagName('tr')
+
+for ( var i = 0; i<weekdays.length; i++){
+
+    var tabelatr1 = document.createElement('tr')
+
+    var thfor = document.createElement('th')
+
+    var textoth = document.createTextNode(weekdays[i])
+
+    thfor.appendChild(textoth)
+
+    tabelatrnobody[0].appendChild(thfor)
+    tabelatrnobody[0].appendChild(tabelatr1)
+
 }
+
+for ( var i = 0; i<4; i++){
+    let tabelatr1 = document.createElement('tr')
+}
+
+console.log(tabelaheadnobody[0]);
+
 
