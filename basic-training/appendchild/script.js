65753ea627aca9242a2e4e1ea232c157fae6ef16
Vinícius Carvalho Ribeiro Quirino <cabohdmiusb@hotmail.com>
2021-04-03 19:02:50 -0300

    New Training 03_04

---
+++ basic-training/appendchild/script.js
@@ -0,0 +1,14 @@
+let days = []
+
+for ( let x = 1; x<32; x++){
+    days.push(Number(x))
+}
+
+let body = document.getElementsByTagName('body')
+
+console.log(days)
+
+function createtr() {
+    let trdays = document.createElement('tr')
+}
+
