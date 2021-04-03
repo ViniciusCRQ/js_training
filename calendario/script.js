57c5993a30e145490314c77990b3234971ac3005
Vinícius Carvalho Ribeiro Quirino <cabohdmiusb@hotmail.com>
2021-04-03 15:28:24 -0300

    Update script.js
    abril ok

--- calendario/script.js
+++ calendario/script.js
@@ -286,42 +286,43 @@ function showtime(){
                     </thead>
                     <tbody>
                         <tr>
+                            <td class="text-muted">28</td>
                             <td class="text-muted">29</td>
                             <td class="text-muted">30</td>
                             <td class="text-muted">31</td>
                             <td>1</td> 
                             <td>2</td>
                             <td>3</td>
-                            <td><a href="https://pt.wikipedia.org/wiki/Páscoa" class="text-danger" target="_blank"><button><strong>4</strong></a></td>
                         </tr>
                         <tr>
+                            <td><a href="https://pt.wikipedia.org/wiki/Páscoa" class="text-danger" target="_blank"><button><strong>4</strong></a></td>
                             <td>5</td>
                             <td>6</td>
                             <td>7</td>
                             <td>8</td>
                             <td>9</td>
                             <td>10</td>
-                            <td>11</td>
                         </tr>
                         <tr>
+                            <td>11</td>
                             <td>12</td>
                             <td>13</td>
                             <td>14</td>
                             <td>15</td>
                             <td>16</td>
                             <td>17</td>
-                            <td>18</td>
                         </tr>
                         <tr>
+                            <td>18</td>
                             <td><a href="https://pt.wikipedia.org/wiki/Dia_do_Índio" class="text-danger" target="_blank"><button><strong>19</strong></a></td>
                             <td>20</td>
                             <td><a href="https://pt.wikipedia.org/wiki/Tiradentes" class="text-danger" target="_blank"><button><strong>21</strong></a></td>
                             <td>22</td>
                             <td>23</td>
                             <td>24</td>
-                            <td>25</td>
                         </tr>
                         <tr>
+                            <td>25</td>
                             <td>26</td>
                             <td>27</td>
                             <td>28</td>
