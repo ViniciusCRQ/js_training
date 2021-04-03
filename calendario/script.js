af90327658a771e3bd4f6431f2c5dd7fc5ea1b73
Vinícius Carvalho Ribeiro Quirino <cabohdmiusb@hotmail.com>
2021-04-03 15:19:07 -0300

    Outubro-Novembro ok

--- calendario/script.js
+++ calendario/script.js
@@ -12,7 +12,7 @@ function showtime(){
     var month = time.getMonth()
     var year = time.getFullYear()   
 
-    month = 7;
+    month = 10;
 
     switch(month){
         case 0 :
@@ -567,7 +567,7 @@ function showtime(){
                 </div> 
                 <div class="col-md-6 col-sm-6>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item text-white bg-primary"><strong>Feriados (Março)</strong></li>
+                    <li class="list-group-item text-white bg-primary"><strong>Feriados (Julho)</strong></li>
                     <li class="list-group-item"><strong>Dia 9</strong> - Dia da revolução constitucionalista</li>
                 </ul>
                 </div>
@@ -687,11 +687,11 @@ function showtime(){
                         <tr>
                             <td>5</td>
                             <td>6</td>
-                            <td>7</td>
-                            <td><button href="#" class="text-primary"><strong>8</strong></td>
-                            <td><button href="#">9</td>
-                            <td><button href="#">10</td>
-                            <td><button href="#">11</td>
+                            <td><a href="https://pt.wikipedia.org/wiki/Dia-dos-Pais" class="text-primary" target="_blank"><button><strong>7</strong></a></td>
+                            <td>8</td>
+                            <td>9</td>
+                            <td>10</td>
+                            <td>11</td>
                         </tr>
                         <tr>
                             <td>12</td>
@@ -725,8 +725,8 @@ function showtime(){
                 </div> 
                 <div class="col-md-6 col-sm-6>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item text-white bg-primary"><strong>Feriados (Março)</strong></li>
-                    <li class="list-group-item"><strong>Dia 8</strong> - Dia internacional da mulher</li>
+                    <li class="list-group-item text-white bg-primary"><strong>Feriados (Setembro)</strong></li>
+                    <li class="list-group-item"><strong>Dia 7</strong> - Dia da Independência do Brasil </li>
                 </ul>
                 </div>
             </div>`
@@ -755,57 +755,67 @@ function showtime(){
                     </thead>
                     <tbody>
                         <tr>
+                            <td class="text-muted">27</td>
+                            <td class="text-muted">28</td>
                             <td class="text-muted">29</td>
                             <td class="text-muted">30</td>
                             <td class="text-muted">31</td>
                             <td>1</td>  
                             <td>2</td>
-                            <td>3</td>
-                            <td>4</td>
                         </tr>
                         <tr>
+                            <td>3</td>
+                            <td>4</td>
                             <td>5</td>
                             <td>6</td>
                             <td>7</td>
                             <td>8</td>
                             <td>9</td>
-                            <td>10</td>
-                            <td>11</td>
                         </tr>
                         <tr>
-                            <td>12</td>
+                            <td>10</td>
+                            <td>11</td>
+                            <td><a href="https://pt.wikipedia.org/wiki/Dia-das-Crianças" class="text-success" target="_blank"><button><strong>12</strong></a></td>
                             <td>13</td>
                             <td>14</td>
                             <td>15</td>
                             <td>16</td>
-                            <td>17</td>
-                            <td>18</td>
                         </tr>
                         <tr>
+                            <td>17</td>
+                            <td>18</td>
                             <td>19</td>
                             <td>20</td>
                             <td>21</td>
                             <td>22</td>
                             <td>23</td>
-                            <td>24</td>
-                            <td>25</td>
                         </tr>
                         <tr>
+                            <td>24</td>
+                            <td>25</td>
                             <td>26</td>
                             <td>27</td>
                             <td>28</td>
                             <td>29</td>
                             <td>30</td>
-                            <td>31</td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://pt.wikipedia.org/wiki/Dia-das-Bruxas" class="text-success" target="_blank"><button><strong>31</strong></a></td>
                             <td class="text-muted">1</td>
+                            <td class="text-muted">2</td>
+                            <td class="text-muted">3</td>
+                            <td class="text-muted">4</td>
+                            <td class="text-muted">5</td>
+                            <td class="text-muted">6</td>
                         </tr>
                     </tbody>
                 </table>
                 </div> 
                 <div class="col-md-6 col-sm-6>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item text-white bg-success"><strong>Feriados (Março)</strong></li>
-                    <li class="list-group-item"><strong>Dia 8</strong> - Dia internacional da mulher</li>
+                    <li class="list-group-item text-white bg-success"><strong>Feriados (Outubro)</strong></li>
+                    <li class="list-group-item"><strong>Dia 12</strong> - Dia das Crianças</li>
+                    <li class="list-group-item"><strong>Dia 31</strong> - Dia das Bruxas</li>
                 </ul>
                 </div>
             </div>`
@@ -834,57 +844,58 @@ function showtime(){
                     </thead>
                     <tbody>
                         <tr>
-                            <td class="text-muted">29</td>
-                            <td class="text-muted">30</td>
                             <td class="text-muted">31</td>
                             <td>1</td>  
-                            <td>2</td>
+                            <td><a href="https://pt.wikipedia.org/wiki/Dia_dos_Fi%C3%A9is_Defuntos" class="text-success" target="_blank"><button><strong>2</strong></a></td>
                             <td>3</td>
                             <td>4</td>
-                        </tr>
-                        <tr>
                             <td>5</td>
                             <td>6</td>
+                        </tr>
+                        <tr>
                             <td>7</td>
                             <td>8</td>
                             <td>9</td>
                             <td>10</td>
                             <td>11</td>
-                        </tr>
-                        <tr>
                             <td>12</td>
                             <td>13</td>
+                        </tr>
+                        <tr>
                             <td>14</td>
-                            <td>15</td>
+                            <td><a href="https://pt.wikipedia.org/wiki/Proclama%C3%A7%C3%A3o_da_Rep%C3%BAblica_do_Brasil" class="text-success" target="_blank"><button><strong>15</strong></a></td>
                             <td>16</td>
                             <td>17</td>
                             <td>18</td>
-                        </tr>
-                        <tr>
                             <td>19</td>
                             <td>20</td>
+                        </tr>
+                        <tr>
                             <td>21</td>
                             <td>22</td>
                             <td>23</td>
                             <td>24</td>
                             <td>25</td>
-                        </tr>
-                        <tr>
                             <td>26</td>
                             <td>27</td>
+                        </tr>
+                        <tr>
                             <td>28</td>
                             <td>29</td>
                             <td>30</td>
-                            <td>31</td>
                             <td class="text-muted">1</td>
+                            <td class="text-muted">2</td>
+                            <td class="text-muted">3</td>
+                            <td class="text-muted">4</td>
                         </tr>
                     </tbody>
                 </table>
                 </div> 
                 <div class="col-md-6 col-sm-6>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item text-white bg-success"><strong>Feriados (Março)</strong></li>
-                    <li class="list-group-item"><strong>Dia 8</strong> - Dia internacional da mulher</li>
+                    <li class="list-group-item text-white bg-success"><strong>Feriados (Novembro)</strong></li>
+                    <li class="list-group-item"><strong>Dia 2</strong> - Dia de Finados</li>
+                    <li class="list-group-item"><strong>Dia 15</strong> - Proclamação da Bandeira</li>
                 </ul>
                 </div>
             </div>`
@@ -946,7 +957,7 @@ function showtime(){
                             <td>22</td>
                             <td>23</td>
                             <td>24</td>
-                            <td>25</td>
+                            <td><a href="https://pt.wikipedia.org/wiki/Natal" class="text-success" target="_blank"><button><strong>25</strong></a></td>
                         </tr>
                         <tr>
                             <td>26</td>
@@ -963,7 +974,7 @@ function showtime(){
                 <div class="col-md-6 col-sm-6>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item text-white bg-success"><strong>Feriados (Março)</strong></li>
-                    <li class="list-group-item"><strong>Dia 8</strong> - Dia internacional da mulher</li>
+                    <li class="list-group-item"><strong>Dia 25</strong> - Natal</li>
                 </ul>
                 </div>
             </div>`
