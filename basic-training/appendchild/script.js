97e69675842837c190ef86c3ff5991f279f53102
Vinícius Carvalho Ribeiro Quirino <cabohdmiusb@hotmail.com>
2021-04-04 01:36:39 -0300

    04-04 First Commit

--- basic-training/appendchild/script.js
+++ basic-training/appendchild/script.js
@@ -1,53 +1,135 @@
-let days = []
-let weekdays = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab']
+//Declaração de Variáveis
 
-for ( let x = 1; x<32; x++){
-    days.push(Number(x))
+const days = []
+const weekdays = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab']
+
+    for ( let x = 1; x<32; x++){
+        days.push(Number(x))
+    }
+
+const tabela = document.createElement('table')
+const body = document.getElementsByTagName('body')
+
+    body[0].appendChild(tabela)
+
+const tabelanobody = document.getElementsByTagName('table');
+const tabelahead = document.createElement('thead');
+const tabelabody = document.createElement('tbody');
+
+const tabelaheadnobody = document.getElementsByTagName('thead')
+const tabelabodynobody = document.getElementsByTagName('tbody')
+
+    tabelanobody[0].appendChild(tabelahead)
+    tabelanobody[0].appendChild(tabelabody)
+ 
+const tabelatr1 = document.createElement('tr')
+const tabelatrnobody = document.getElementsByTagName('tr')
+
+    tabelatr1.id = 'TabelaNomesSemana'
+
+    tabelaheadnobody[0].appendChild(tabelatr1)
+
+// Nomes dos dias da semana
+function headercalendario(){
+    for ( var i = 0; i<weekdays.length; i++){
+
+        var thdiassemana = document.createElement('th')
+        var textothdiassemana = document.createTextNode(weekdays[i])
+
+        thdiassemana.appendChild(textothdiassemana)
+
+        tabelatrnobody[0].appendChild(thdiassemana)
+    }
 }
 
-let tabela = document.createElement('table')
 
-let body = document.getElementsByTagName('body')
+// Tabelas relacionadas com as Semanas
+function linhassemanas(){
+    for ( var i = 0; i<5; i++){
 
-body[0].appendChild(tabela)
+        let tabelatrsemanas = document.createElement(`tr`)
+        tabelatrsemanas.id = `semana${i}`
+        let textotr = document.createTextNode('')
+    
+        tabelatrsemanas.appendChild(textotr)
+        tabelabodynobody[0].appendChild(tabelatrsemanas)
 
-let tabelanobody = document.getElementsByTagName('table');
+    }
+}
 
-let tabelahead = document.createElement('thead');
+function preenchercalendario() {
+    
+    let semana0 = document.getElementById('semana0')
+    let semana1 = document.getElementById('semana1')
+    let semana2 = document.getElementById('semana2')
+    let semana3 = document.getElementById('semana3')
+    let semana4 = document.getElementById('semana4')
 
-let tabelabody = document.createElement('tbody');
+    console.log(semana0)
 
-let tabelaheadnobody = document.getElementsByTagName('thead')
-let tabelabodynobody = document.getElementsByTagName('tbody')
+    for ( var i = 1; i<days.length; i++){
 
-tabelanobody[0].appendChild(tabelahead)
+        if ( i < 8 ){
 
+            var thtotaldays =  document.createElement('th')
+            var textothtotaldays = document.createTextNode(i)
 
-let tabelatr = document.createElement('tr')
-   
-tabelaheadnobody[0].appendChild(tabelatr)
+            thtotaldays.appendChild(textothtotaldays)
 
-let tabelatrnobody = document.getElementsByTagName('tr')
+            semana0.appendChild(thtotaldays)
 
-for ( var i = 0; i<weekdays.length; i++){
+    } else if ( i < 15 ) {
 
-    var tabelatr1 = document.createElement('tr')
+        var thtotaldays =  document.createElement('th')
+        var textothtotaldays = document.createTextNode(i)
 
-    var thfor = document.createElement('th')
+        thtotaldays.appendChild(textothtotaldays)
 
-    var textoth = document.createTextNode(weekdays[i])
+        semana1.appendChild(thtotaldays)
 
-    thfor.appendChild(textoth)
+    } else if ( i < 22 ) {
+        var thtotaldays =  document.createElement('th')
+        var textothtotaldays = document.createTextNode(i)
 
-    tabelatrnobody[0].appendChild(thfor)
-    tabelatrnobody[0].appendChild(tabelatr1)
+        thtotaldays.appendChild(textothtotaldays)
+
+        semana2.appendChild(thtotaldays)
+    } else if ( i < 29 ) {
+        var thtotaldays =  document.createElement('th')
+        var textothtotaldays = document.createTextNode(i)
+
+        thtotaldays.appendChild(textothtotaldays)
+
+        semana3.appendChild(thtotaldays)
+    } else {
+        var thtotaldays =  document.createElement('th')
+        var textothtotaldays = document.createTextNode(i)
+
+        thtotaldays.appendChild(textothtotaldays)
+
+        semana4.appendChild(thtotaldays)
+    }
 
 }
 
-for ( var i = 0; i<4; i++){
-    let tabelatr1 = document.createElement('tr')
+    for ( let i = 1; i<6; i++ ){
+        var thtotaldays =  document.createElement('th')
+        var textothtotaldays = document.createTextNode(i)
+
+        thtotaldays.appendChild(textothtotaldays)
+
+        semana4.appendChild(thtotaldays)
+        thtotaldays.style.color = '#C0C0C0'
+    }
+
 }
 
+headercalendario();
+linhassemanas();
+preenchercalendario();
+
+
+
+console.log(tabelabodynobody[0]);
 console.log(tabelaheadnobody[0]);
-
-
+console.log(tabelanobody[0])
